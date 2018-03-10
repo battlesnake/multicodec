cd0d9c817d6a3d0272af329dbc77067b0443f085
Mark K Cowan <mark@battlesnake.co.uk>
2018-03-10 16:40:20 +0000

    java: added support for codec-tagged data

--- java/src/main/java/com/kuckian/multicodec/util/StringTransform.java
+++ java/src/main/java/com/kuckian/multicodec/util/StringTransform.java
@@ -7,7 +7,11 @@ import com.kuckian.multicodec.Direction;
 import com.kuckian.multicodec.InvalidCodingSchemeException;
 import com.kuckian.multicodec.InvalidOptionsException;
 
+/* TODO: Optimise out the excessive copyslicing */
 public abstract class StringTransform {
+
+	public static final char tagDelim = ':';
+	public static final int MAX_TAGGED_SCHEME_LENGTH = 10000;
 
 	public static String encode(String scheme, byte[] input)
 			throws IOException, InvalidOptionsException, InvalidCodingSchemeException {
@@ -21,4 +25,20 @@ public abstract class StringTransform {
 				input.getBytes("utf-8"));
 	}
 
+	public static String encodeTagged(String scheme, byte[] input)
+			throws IOException, InvalidOptionsException, InvalidCodingSchemeException {
+		return scheme + tagDelim + new String(
+				MulticodecTransform.encodeBytes(CodingScheme.deserialize(scheme, Direction.ENCODE), input), "utf-8");
+	}
+
+	public static byte[] decodeTagged(String input)
+			throws IOException, InvalidOptionsException, InvalidCodingSchemeException {
+		int delim = input.indexOf(tagDelim);
+		if (delim == -1 || delim > MAX_TAGGED_SCHEME_LENGTH) {
+			throw new InvalidCodingSchemeException("Failed to find tagged coding scheme in input data");
+		}
+		String scheme = input.substring(0, delim);
+		byte[] data = input.substring(delim + 1).getBytes("utf-8");
+		return MulticodecTransform.decodeBytes(CodingScheme.deserialize(scheme, Direction.DECODE), data);
+	}
 }
--- java/src/test/java/com/kuckian/multicodec/test/codec/StringTransformTest.java
+++ java/src/test/java/com/kuckian/multicodec/test/codec/StringTransformTest.java
@@ -12,11 +12,21 @@ import com.kuckian.multicodec.util.StringTransform;
 
 public class StringTransformTest {
 
+	private static final String scheme = "gzip(mode=size)+base64";
+
 	@Test
-	public final void test() throws IOException, InvalidOptionsException, InvalidCodingSchemeException {
+	public final void testBasic() throws IOException, InvalidOptionsException, InvalidCodingSchemeException {
 		byte[] rawInputData = TestUtils.TEST_STRING.getBytes("utf-8");
-		String wireData = StringTransform.encode("gzip+base64", rawInputData);
-		byte[] rawDecodedData = StringTransform.decode("gzip+base64", wireData);
+		String wireData = StringTransform.encode(scheme, rawInputData);
+		byte[] rawDecodedData = StringTransform.decode(scheme, wireData);
+		assertArrayEquals(rawInputData, rawDecodedData);
+	}
+
+	@Test
+	public final void testTagged() throws IOException, InvalidOptionsException, InvalidCodingSchemeException {
+		byte[] rawInputData = TestUtils.TEST_STRING.getBytes("utf-8");
+		String wireData = StringTransform.encodeTagged(scheme, rawInputData);
+		byte[] rawDecodedData = StringTransform.decodeTagged(wireData);
 		assertArrayEquals(rawInputData, rawDecodedData);
 	}
 
